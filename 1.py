fbedf1c097ae7085f776d688aa0049ddf76fa619
Chai Nunes <chaiteanunes@gmail.com>
2016-12-19 08:15:59 -0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+# Multiples of 3 and 5
+sum = 0
+
+def is_of_3(n):
+    if n % 3 == 0:
+        return True
+    return False
+
+def is_of_5(n):
+    if n % 5 == 0:
+        return True
+    return False
+    
+for i in range(0, 1000):
+   if is_of_3(i) or is_of_5(i):
+      sum += i
+
+print(sum)
