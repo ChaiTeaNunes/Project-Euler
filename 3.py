8195dfd42eb842ca6e4c1cd486ceb7663cf1d95e
Chai Nunes <chaiteanunes@gmail.com>
2016-12-20 08:59:39 -0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 # Largest Prime Factor
 def largest_prime(n):
-   i = 2
-   while i**2 < n:
-      while n % i == 0:
-         n /= i
-      i += 1
-   return n
+    i = 2
+    while i**2 < n:
+       while n % i == 0:
+          n /= i
+       i += 1
+    return n
 
 print(largest_prime(600851475143))
