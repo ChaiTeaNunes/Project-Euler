496a45097a3d37319be09dfbf8acbf28f8d9601d
Chai Nunes <chaiteanunes@gmail.com>
2016-12-21 09:07:06 -0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,17 @@
-# Largest Palindrome Project
+# Largest Palindrome Product
 def largest_palindrome():
-    i, start, end = 100, 100, 999
-    output = str()
+    start, end = 100, 1000
+    value, output = 0, 0
+    
+    for i in range(start, end):
+        for j in range(i, end):
+            value = i * j
+            temp = str()
+            for k in range(len(str(value)) - 1, -1, -1):
+                temp += str(value)[k]
+            if temp == str(value) and value >= output:
+                output = value
+            del temp
+    print(output)
+
+largest_palindrome()
