8b561b8c70d7f34b12a806aa4bcc087e9e00d886
Chai Nunes <chaiteanunes@gmail.com>
2016-12-21 08:56:09 -0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,10 +6,10 @@ next_fib = 0
 sum = 0
 
 while (sum <= MAX):
-   next_fib = current_fib + prev_fib
-   if next_fib % 2 == 0:
-      sum += next_fib
-   prev_fib = current_fib
-   current_fib = next_fib
+    next_fib = current_fib + prev_fib
+    if next_fib % 2 == 0:
+       sum += next_fib
+    prev_fib = current_fib
+    current_fib = next_fib
 
 print(sum)
