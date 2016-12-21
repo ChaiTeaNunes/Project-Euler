96929b21da769db2623f0df73cf4246fc603800f
Chai Nunes <chaiteanunes@gmail.com>
2016-12-21 08:57:04 -0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,9 +2,9 @@
 def largest_prime(n):
     i = 2
     while i**2 < n:
-       while n % i == 0:
-          n /= i
-       i += 1
+        while n % i == 0:
+            n /= i
+        i += 1
     return n
 
 print(largest_prime(600851475143))
