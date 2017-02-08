2d8a57c9792ff9d2737dd46251748426a265e009
Chai Nunes <chaiteanunes@gmail.com>
2017-02-08 08:39:55 -0800

    Fix bug

--- python/1-9/3.py
+++ python/1-9/3.py
@@ -1,8 +1,10 @@
 # Largest Prime Factor
 def largest_prime(n):
     i = 2
-    while i**2 < n:
+    while i**2 <= n:
         while n % i == 0:
+            if (n / i == 1):
+                break
             n /= i
         i += 1
     return n
