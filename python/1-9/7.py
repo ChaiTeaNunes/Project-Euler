8dd226e5b74a5201970fa02276270823bef710a9
Chai Nunes <chaiteanunes@gmail.com>
2017-03-08 09:01:42 -0800

    Update 7.py

--- python/1-9/7.py
+++ python/1-9/7.py
@@ -1,5 +1,24 @@
 # 10001st prime
-def nth_prime(n):
+import math
+
+def next_prime(n):
+    end = math.sqrt(n) + 1
+    for mod in range(2, int(end)):
+        if n % mod == 0:
+            return next_prime(n + 1)
     return n
+
+def get_prime_list(n):
+    count = 1
+    prime = 2
+    prime_list = list()
+    while count <= n:
+        prime_list.append(next_prime(prime))
+        prime = next_prime(prime) + 1
+        count += 1
+    return prime_list
+
+def nth_prime(n):
+    return get_prime_list(n)[n - 1]
 
 print(nth_prime(10001))
