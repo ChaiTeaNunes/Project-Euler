a599edcb918dd58b0378af2c5432c2eff442709e
Chai Nunes <chaiteanunes@gmail.com>
2017-03-09 08:57:01 -0800

    Update 9.py

--- python/1-9/9.py
+++ python/1-9/9.py
@@ -1,7 +1,22 @@
 #Special Pythagorean Triplet
-def is_triplet(a, b, c):
+import math
+
+def is_triple(a, b, c):
     return a**2 + b**2 == c**2
 
-def triplet_sum(a, b, c):
-    if is_triplet(a, b, c):
+def triple_sum(a, b, c):
+    if is_triple(a, b, c):
         return a + b + c
+    else:
+        return -1
+
+def find_triple_sum(n):
+    for c in range(1, n - 3): # a could be 1 but no less and b could be 2 but no less
+        for b in range(1, n - c):
+            a = n - b - c
+            if a > b:
+                a, b = b, a
+            if triple_sum(a, b, c) == n:
+                return str(a) + ", " + str(b) + ", " + str(c)
+
+print(find_triple_sum(1000))
