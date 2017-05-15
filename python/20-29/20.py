c7b37adccc427dcb5adfbbc7612ac4697a6347e3
Chai Nunes <chaiteanunes@gmail.com>
2017-05-15 08:58:56 -0700

    Update 20.py

--- python/20-29/20.py
+++ python/20-29/20.py
@@ -1,1 +1,12 @@
+# Factorial Digit Sum
 
+def factorial(n):
+    output = 1
+    for i in range(n):
+        output *= (i+1)
+    return output
+    
+def sum_digits(n):
+    return sum(map(int, list(str(n))))
+
+print(sum_digits(factorial(100)))
