a3bb2489c5646d6622d1d8a19790295bf4a7c6b5
Chai Nunes <chaiteanunes@gmail.com>
2017-05-25 16:33:39 -0700

    Update 24.py

--- python/20-29/24.py
+++ python/20-29/24.py
@@ -2,23 +2,26 @@
 
 def perms(n):
     output = 1
-    for i in range(0, n):
+    for i in range(n):
         output += output * (i+1)
     return output
-
+            
 def nth_perm(n, nums):
     if isinstance(nums, int):
         nums = [i for i in range(nums+1)]
-        
-    size = len(nums)-1
-    if size <= 0:
-        return str(nums[0])
-    for i, num in enumerate(nums):
-        if n <= (i+1) * perms(size-1):
-            n -= i * perms(size-1)
-            nums.remove(num)
-            return str(num) + nth_perm(n, nums)
     
-    return str(nums[0])
+    size = len(nums)
+    if size == 1:
+        return str(nums[0])
+    elif size > 1:
+        num = int(n / perms(size-2))
+        if n % perms(size-2) == 0:
+            num -= 1
+        digit = nums[num]
+        n -= (num * perms(size-2))
+        nums.remove(digit)
+        return str(digit) + nth_perm(n, nums)
+    else:
+        return str()
 
 print(nth_perm(1000000, 9))
