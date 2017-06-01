2610880aa32bd0c0b77878d53986cbfdf9a4262d
Chai Nunes <chaiteanunes@gmail.com>
2017-06-01 08:34:51 -0700

    Update 30.py

--- python/30-39/30.py
+++ python/30-39/30.py
@@ -1,1 +1,14 @@
+# Digit Fifth Powers
 
+def is_power_sum(n):
+    digits = [int(i)**5 for i in list(str(n))]
+    return sum(digits) == n
+
+def digit_fifth_powers():
+    output = list()
+    for i in range(2, 1000000):
+        if is_power_sum(i):
+            output.append(i)
+    return output
+
+print(sum(digit_fifth_powers()))
