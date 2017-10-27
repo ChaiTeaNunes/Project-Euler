7396856adff019d2f128e96618e28c237394af0e
Chai Nunes <chaiteanunes@gmail.com>
2017-10-27 09:27:07 -0700

    Update 15.py

--- python/10-19/15.py
+++ python/10-19/15.py
@@ -2,9 +2,9 @@
 
 def paths(size=1):
     output = [1] * (size + 1)
-	for i in range(size):
-		for j in range(size):
-			output[j+1] = output[j] + output[j+1]
-	return output[-1]
+    for i in range(size):
+        for j in range(size):
+            output[j+1] = output[j] + output[j+1]
+    return output[-1]
 
 print(paths(20))
