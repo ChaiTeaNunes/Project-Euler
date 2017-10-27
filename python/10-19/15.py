6b9addfe2f774886662c011fb4b6139c13e3f281
Chai Nunes <chaiteanunes@gmail.com>
2017-10-27 10:20:52 -0700

    Optimize Solution

--- python/10-19/15.py
+++ python/10-19/15.py
@@ -4,7 +4,7 @@ def paths(size=1):
     output = [1] * (size + 1)
     for i in range(size):
         for j in range(size):
-            output[j+1] = output[j] + output[j+1]
+            output[j+1] += output[j]
     return output[-1]
 
 print(paths(20))
