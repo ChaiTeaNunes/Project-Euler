1fb46029c5b982a83ecc18b4c80b06de87215a25
Chai Nunes <chaiteanunes@gmail.com>
2017-10-27 09:24:36 -0700

    Optimize Solution

--- python/10-19/15.py
+++ python/10-19/15.py
@@ -1,12 +1,10 @@
 #Lattice Paths
 
 def paths(size=1):
-    pascal = [[1] * (size + 1)]
-    for i in range(1, size + 1):
-        temp = [1]
-        for j in range(1, size + 1):
-            temp.append(temp[j-1] + pascal[i-1][j])
-        pascal.append(temp)
-    return pascal[-1][-1]
+	output = [1] * (size + 1)
+	for i in range(size):
+		for j in range(size):
+			output[j+1] = output[j] + output[j+1]
+	return output[-1]
 
 print(paths(20))
