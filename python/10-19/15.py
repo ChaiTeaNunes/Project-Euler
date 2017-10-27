947fc939bc602ab5523456b73489e445bee2f993
Chai Nunes <chaiteanunes@gmail.com>
2017-10-27 09:25:34 -0700

    Update 15.py

--- python/10-19/15.py
+++ python/10-19/15.py
@@ -1,7 +1,7 @@
 #Lattice Paths
 
 def paths(size=1):
-	output = [1] * (size + 1)
+    output = [1] * (size + 1)
 	for i in range(size):
 		for j in range(size):
 			output[j+1] = output[j] + output[j+1]
